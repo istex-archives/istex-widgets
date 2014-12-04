5d6a3517cbc732772fbdc794cdccfc0d7750f335
Stéphane Gully <stephane.gully@gmail.com>
2014-12-04 22:30:49 +0100

    test the request result

--- test/search.spec.js
+++ test/search.spec.js
@@ -1,6 +1,6 @@
 var httpServer = require('http-server');
 var Browser    = require('zombie');
-var assert     = require('assert');
+var assert     = require('chai').assert;
 
 describe('Istex search widget', function () {
   before(function (done) {
@@ -32,7 +32,15 @@ describe('Istex search widget', function () {
     assert.equal(this.browser.query('input.istex-search-submit').type, 'submit');    
   });
 
-  it('should receive data when the query is submitted');
+  it('should receive data when the query is submitted', function (done) {
+    var self = this;
+    assert.ok(self.browser.success);
+    self.browser.fill('input.istex-search-input', 'brain');
+    self.browser.pressButton('Rechercher').then(function () {
+      assert.ok(self.browser.success);
+      assert.include(self.browser.text('#istex-results-in-dom'), '"total":');
+    }).then(done, done);
+  });
   
   after(function () {
     this.server.close();
