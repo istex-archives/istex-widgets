c77d047d1831ec5a3fb4331c2f040eba88b48657
Stéphane Gully <stephane.gully@gmail.com>
2014-12-15 17:03:16 +0100

    add the checkIsAuthWithCookie function in charge of opening a window to force cookie authentification system (ex: ezproxy)

--- search/js/script.js
+++ search/js/script.js
@@ -18,11 +18,26 @@
     this.init();
   }
 
+  /**
+   * Try to authenticate with a cookie if needed
+   * then load the input query form
+   */
   Plugin.prototype.init = function () {
+    var self = this;
+    self.checkIsAuthWithCookie(function (err) {
+      self.loadInputForm();
+    });
+  };
+
+  /**
+   * Load the input query form
+   */
+  Plugin.prototype.loadInputForm = function () {
     var self = this;
 
     /*jshint ignore:start*/
     // insert the form search into the DOM
+    $(self.elt).empty();
     $(self.elt).append(
       '<form class="istex-search-form">' +
         '<div class="istex-search-bar-wrapper">' +
@@ -60,7 +75,7 @@
         },
         error: function (opt, err) {
           $(self.elt).find('.istex-search-error').html(
-            '<a href="https://api.istex.fr/document/?q=*">API Istex</a> non joignable.'
+            '<a href="https://api.istex.fr/corpus/">API Istex</a> non joignable.'
           );
           $(self.elt).find('.istex-search-error').show();
         }
@@ -74,6 +89,49 @@
       $(self.elt).find('.istex-search-form').trigger('submit');
     }
 
+  };
+
+  /**
+   * Open a popup if necessary to authenticate the user
+   * through a cookie system (example: ezproxy)
+   */
+  Plugin.prototype.checkIsAuthWithCookie = function (cb) {
+    var self = this;
+
+    // prepare a connect button and open a the
+    // cookie auth window if clicked
+    var authPopup = null;
+    function insertConnectButtonIfNotExists() {
+      if ($(self.elt).find('.istex-ezproxy-auth-btn').length > 0) return;
+      $(self.elt).append(
+        '<button class="istex-ezproxy-auth-btn">Se connecter<div></div></button>'
+      );
+      $(self.elt).find('.istex-ezproxy-auth-btn').click(function () {
+        // open a new window on the corpus route to force authentication
+        authPopup = window.open(self.settings.istexApi + '/corpus/');
+      });      
+    }
+
+    // request on corpus route in order to
+    // test if auth is ok or not
+    function authChecker() {
+      $.jsonp({
+        url: self.settings.istexApi + '/corpus/',
+        callbackParameter: "callback",
+        success: function(corpus) {
+          // auth is ok when corpus list can be retrived from the istex api
+          if (authPopup) authPopup.close();
+          cb(null);
+        },
+        error: function (opt, err) {
+          // if not yet auth, insert a button
+          insertConnectButtonIfNotExists();
+          // if not yet auth, try again later
+          setTimeout(authChecker, 100);
+        }
+      });
+    };
+    authChecker();
   };
 
   // A really lightweight plugin wrapper around the constructor,
