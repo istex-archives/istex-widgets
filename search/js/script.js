5c873d6666281b9df9dc239d40abf16d495f815e
Stéphane Gully <stephane.gully@gmail.com>
2015-01-05 16:07:05 +0100

    basic login/password popup used for standard http authentication (basic auth)

--- search/js/script.js
+++ search/js/script.js
@@ -104,35 +104,102 @@
    */
   Plugin.prototype.authWithRedirect = function (cb) {
     var self = this;
-    insertNewWinConnectButtonIfNotExists();
 
     // prepare a connect button and open a the
     // cookie auth window if clicked
-    var authPopup = null;
-    function insertNewWinConnectButtonIfNotExists() {
-      if ($(self.elt).find('.istex-ezproxy-auth-btn').length > 0) return;
-      $(self.elt).html(
-        '<button class="istex-ezproxy-auth-btn">Se connecter<div></div></button>'
-      );
-      $(self.elt).find('.istex-ezproxy-auth-btn').click(function () {
-        // open the window on the corpus route to request authentication
-        authPopup = window.open(self.settings.istexApi + '/ezproxy-auth-and-close.html');
+    self.insertConnectBtnIfNotExists(function () {
+      // open the window on the corpus route to request authentication
+      authPopup = window.open(self.settings.istexApi + '/ezproxy-auth-and-close.html');
 
-        // check again auth when the user come back on the origin page
-        $(window).focus(function () {
-          console.log('focus', authPopup);
-          authPopup = null;
-          self.getAuthMode(function (err, needAuth, authMode) {
-            if (needAuth == 'none') {
-              cb(null);
-            } else {
-              cb(new Error("Unable to authenticate"));
+      // check again auth when the user come back on the origin page
+      $(window).focus(function () {
+        self.getAuthMode(function (err, needAuth, authMode) {
+          if (needAuth == 'none') {
+            cb(null);
+          } else {
+            cb(new Error("Unable to authenticate"));
+          }
+        });
+      });
+
+    });
+  };
+
+  /**
+   * Insert a connect button in the page if it not exists yet
+   * - cb is called when the button is clicked
+   */
+  Plugin.prototype.insertConnectBtnIfNotExists = function (cb) {
+    var self = this;
+    if ($(self.elt).find('.istex-ezproxy-auth-btn').length > 0) return;
+    $(self.elt).append(
+      '<button class="istex-ezproxy-auth-btn">Se connecter<div></div></button>'
+    );
+    $(self.elt).find('.istex-ezproxy-auth-btn').click(cb);
+  };
+
+  /**
+   * Authenticate with the standard HTTP basic auth
+   * - shows a HTML popup to ask login and password
+   * - when submitted, try to login through AJAX
+   * - if auth ok, then close the popup and return credentials to cb
+   * - if auth ok, then show an error message
+   */
+  Plugin.prototype.authWithHTTP = function (cb) {
+    var self = this;
+
+    // first of all insert the connect button and when
+    // it is clicked, then show the login/password popup
+    self.insertConnectBtnIfNotExists(function () {
+      if ($(self.elt).find('.istex-auth-popup').length > 0) return;
+
+      // append a simple login/password popup
+      $(self.elt).append(
+      '<form class="istex-auth-popup">' +
+        '<div class="istex-auth-popup-wrapper">' +
+          '<input class="istex-auth-popup-login" type="text" value="" placeholder="Votre login ..." />' +
+          '<input class="istex-auth-popup-password" type="password" value="" placeholder="Votre mot de passe ..." />' +
+          '<input class="istex-auth-popup-cancel" type="submit" value="Annuler" />' +
+          '<input class="istex-auth-popup-submit" type="submit" value="Se connecter" />' +
+        '</div>' +
+        '<p class="istex-auth-popup-error"></p>' +
+      '</form>'
+      );
+
+      $(self.elt).find('.istex-auth-popup').submit(function () {
+        var clicked = $(self.elt).find(".istex-auth-popup input[type=submit]:focus")
+                                 .attr('class');
+        if (clicked == 'istex-auth-popup-submit') {
+          // if "Se connecter" is clicked, then try to auth through AJAX
+          // with the given login/password
+          var httpOptions = {
+            username: $(self.elt).find('istex-auth-popup-login').val(),
+            password: $(self.elt).find('istex-auth-popup-password').val(),
+          };
+          $.ajax({
+            url: self.settings.istexApi + '/corpus/',
+            username: httpOptions.username,
+            password: httpOptions.password,
+            success: function () {
+              // auth ok, then cleanup and respond ok
+              $(self.elt).find('.istex-auth-popup').remove();
+              return cb(null, httpOptions);
+            },
+            error: function (opt, err) {
+              $(self.elt).find('.istex-auth-popup-error')
+                         .text("Le nom d'utilisateur ou le mot de passe saisi est incorrect.")
             }
           });
-        });
-
+        } else if (clicked == 'istex-auth-popup-cancel') {
+          // if "Annuler" button is clicked, then cleanup
+          $(self.elt).find('.istex-auth-popup').remove();
+          cb(new Error('HTTP auth canceled'));
+        }
+        return false;
       });
-    }
+    
+    });
+
   };
 
 
@@ -183,7 +250,7 @@
             '<input class="istex-search-input" type="text" value="" placeholder="Votre requête ici ..." />' +
           '</span>' +
         '</div>' +
-        '<p class="istex-search-error">error</p>' +
+        '<p class="istex-search-error"></p>' +
       '</form>'
     );
     /*jshint ignore:end*/
