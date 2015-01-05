7b684cf6eee945e7cef12f4e8014aa1b62770ad5
Stéphane Gully <stephane.gully@gmail.com>
2015-01-05 15:10:20 +0100

    new way th handle ezproxy auth

--- search/js/script.js
+++ search/js/script.js
@@ -24,9 +24,146 @@
    */
   Plugin.prototype.init = function () {
     var self = this;
-    self.checkIsAuthWithCookie(function (err) {
-      self.loadInputForm();
+
+    // first of all, check which auth system is available
+    self.getAuthMode(function (err, needAuth, authMode) {
+      if (needAuth == 'none') {
+        self.setupGenericRequester(authMode);
+        // auth is ok, then load the user interface
+        self.loadInputForm();
+      } else if (needAuth == 'redirect') {
+        // auth with the redirect method
+        self.authWithRedirect(function (err) {
+          if (!err) {
+            self.setupGenericRequester(authMode);
+            // auth is ok, then load the user interface
+            self.loadInputForm();
+          }
+        });
+      } else if (needAuth == 'http') {
+        // auth with the standard http method
+        self.authWithHTTP(function (err, options) {
+          if (!err) {
+            self.setupGenericRequester(authMode, options);
+            // auth is ok, then load the user interface
+            self.loadInputForm();
+          }
+        });
+      }
     });
+  };
+
+
+  /**
+   * Return which auth method is used.
+   * 2nd cb parameter value could be
+   * - none: if authorized (then 3rd parameter is given)
+   * - http: if http basic auth is requested
+   * - redirect: if a redirected (ex: ezproxy) auth is requested
+   * 3d cb parameter value could be
+   * - ajax
+   * - jsonp
+   */
+  Plugin.prototype.getAuthMode = function (cb) {
+    var self = this;
+
+   // try to auth on the API with AJAX
+    $.ajax({
+      url: self.settings.istexApi + '/corpus/',
+      success: function () {
+        // if success it means auth is ok
+        return cb(null, 'none', 'ajax');
+      },
+      error: function (opt, err) {
+        
+        // 0 means cross domain security error caused by ezproxy
+        // 302 means redirection caused by ezproxy
+        if (opt.status == 0 || opt.status == 302) {
+          // try to auth on the API with JSONP
+          $.jsonp({
+            url: self.settings.istexApi + '/corpus/',
+            callbackParameter: "callback",
+            success: function () {
+              cb(null, 'none', 'jsonp');
+            },
+            error: function () {
+              cb(null, 'redirect', 'jsonp');   
+            }
+          });
+        } else {
+          // other code are interpreted as 401
+          return cb(null, 'http', 'ajax');
+        }
+      }
+    });
+  };
+
+  /**
+   * Open a new window to authenticate the user
+   * through a cookie system (example: ezproxy)
+   */
+  Plugin.prototype.authWithRedirect = function (cb) {
+    var self = this;
+    insertNewWinConnectButtonIfNotExists();
+
+    // prepare a connect button and open a the
+    // cookie auth window if clicked
+    var authPopup = null;
+    function insertNewWinConnectButtonIfNotExists() {
+      if ($(self.elt).find('.istex-ezproxy-auth-btn').length > 0) return;
+      $(self.elt).html(
+        '<button class="istex-ezproxy-auth-btn">Se connecter<div></div></button>'
+      );
+      $(self.elt).find('.istex-ezproxy-auth-btn').click(function () {
+        // open the window on the corpus route to request authentication
+        authPopup = window.open(self.settings.istexApi + '/ezproxy-auth-and-close.html');
+
+        // check again auth when the user come back on the origin page
+        $(window).focus(function () {
+          console.log('focus', authPopup);
+          authPopup = null;
+          self.getAuthMode(function (err, needAuth, authMode) {
+            if (needAuth == 'none') {
+              cb(null);
+            } else {
+              cb(new Error("Unable to authenticate"));
+            }
+          });
+        });
+
+      });
+    }
+  };
+
+
+  /**
+   * Create a self.requester function
+   * used to wrap jsonp or ajax query system
+   */
+  Plugin.prototype.setupGenericRequester = function (authMode, options) {
+    var self = this;
+
+    // create a generic requester
+    // based on jsonp or ajax
+    if (authMode  == 'jsonp') {
+      // jsonp
+      self.requester = function (options) {
+        var reqOpt = $.extend({
+          callbackParameter: "callback"
+        }, options);
+        return $.jsonp(reqOpt);
+      };
+    } else {
+      // ajax
+      self.requester = function (options) {
+        var reqOpt = $.extend({
+          headers: {
+            // todo : basic auth <= options
+          }
+        }, options);
+        return $.ajax(reqOpt);
+      };
+    }
   };
 
   /**
@@ -60,13 +197,11 @@
       var query = $(self.elt).find('input.istex-search-input').val().trim();
       query = query ? query : '*';
 
-      // send the request to the istex api with the
-      // jquery-jsonp lib because errors are not
-      // handled by the native jquery jsonp function
-      $.jsonp({
+      // send the request to the istex api
+      self.requester({
         url: self.settings.istexApi + '/document/',
         data: { q: query, output: '*' },
-        callbackParameter: "callback",
+        //callbackParameter: "callback",
         success: function(items) {
           // hide the error box
           $(self.elt).find('.istex-search-error').hide();
@@ -92,52 +227,101 @@
   };
 
   /**
+   * Check if standard HTTP auth needed
+   */
+  // Plugin.prototype.tryAuthWithHTTP = function (cb) {
+  //   var self = this;
+
+  //   function insertHTTPConnectButtonIfNotExists(cbAuth) {
+  //     if ($(self.elt).find('.istex-ezproxy-auth-btn').length > 0) return;
+  //     $(self.elt).append(
+  //       '<button class="istex-ezproxy-auth-btn">Se connecter<div></div></button>'
+  //     );
+  //     $(self.elt).find('.istex-ezproxy-auth-btn').click(function () {
+  //       // try to popup the HTTP auth dialog
+  //       $.jsonp({
+  //         url: self.settings.istexApi + '/corpus/',
+  //         callbackParameter: "callback",
+  //         success: function (corpus) {
+  //           cbAuth(null);
+  //         },
+  //         error: function (opt, err) {
+  //           cbAuth(new Error('http auth error'));
+  //         }
+  //       });
+  //     });      
+  //   }
+
+  //   // test if API is open
+  //   $.ajax({
+  //     url: self.settings.istexApi + '/corpus/',
+  //     success: function (corpus) {
+  //       // auth is ok, it means ne pre-autentication is needed 
+  //       cb(null);
+  //     },
+  //     error: function (opt, err) {
+  //       // if not yet auth, insert a connect button
+  //       insertConnectButtonIfNotExists(function (err) {
+
+  //       });
+  //     }
+  //   });
+
+  // };
+
+  /**
    * Open a popup if necessary to authenticate the user
    * through a cookie system (example: ezproxy)
    */
-  Plugin.prototype.checkIsAuthWithCookie = function (cb) {
-    var self = this;
+  // Plugin.prototype.tryAuthWithCookie = function (httpAuth, cb) {
+  //   var self = this;
 
-    // prepare a connect button and open a the
-    // cookie auth window if clicked
-    var authPopup = null;
-    function insertConnectButtonIfNotExists() {
-      if ($(self.elt).find('.istex-ezproxy-auth-btn').length > 0) return;
-      $(self.elt).append(
-        '<button class="istex-ezproxy-auth-btn">Se connecter<div></div></button>'
-      );
-      $(self.elt).find('.istex-ezproxy-auth-btn').click(function () {
-        // open a new window on the corpus route to force authentication
-        authPopup = window.open(self.settings.istexApi + '/corpus/');
-      });      
-    }
+  //   // prepare a connect button and open a the
+  //   // cookie auth window if clicked
+  //   var authPopup = null;
+  //   function insertNewWinConnectButtonIfNotExists() {
+  //     if ($(self.elt).find('.istex-ezproxy-auth-btn').length > 0) return;
+  //     $(self.elt).append(
+  //       '<button class="istex-ezproxy-auth-btn">Se connecter<div></div></button>'
+  //     );
+  //     $(self.elt).find('.istex-ezproxy-auth-btn').click(function () {
+  //       if (!httpAuth) {
+  //         // open a new window on the corpus route to force authentication
+  //         authPopup = window.open(self.settings.istexApi + '/corpus/');          
+  //       }
+  //       // run the auth checker
+  //       authChecker();
+  //     });      
+  //   }
 
-    // request on corpus route in order to
-    // test if auth is ok or not
-    function authChecker() {
-      $.jsonp({
-        url: self.settings.istexApi + '/corpus/',
-        callbackParameter: "callback",
-        success: function(corpus) {
-          // auth is ok when corpus list can be retrived from the istex api
-          if (authPopup) authPopup.close();
-          cb(null);
-        },
-        error: function (opt, err) {
-          // if not yet auth, insert a button
-          insertConnectButtonIfNotExists();
-          // if not yet auth, try again later
-          setTimeout(authChecker, 100);
-        }
-      });
-    };
-    authChecker();
-  };
+  //   // request on corpus route in order to
+  //   // test if auth is ok or not
+  //   function authChecker() {
+  //     $.jsonp({
+  //       url: self.settings.istexApi + '/corpus/',
+  //       callbackParameter: "callback",
+  //       success: function (corpus) {
+  //         // auth is ok when corpus list can be retrived from the istex api
+  //         if (authPopup) authPopup.close();
+  //         cb(null);
+  //       },
+  //       error: function (opt, err) {
+  //         // if not yet auth, insert a button
+  //         insertNewWinConnectButtonIfNotExists();
+          
+  //         if (httpAuth) {
+  //           // if not yet auth, try again later
+  //           setTimeout(authChecker, 100);            
+  //         }
+  //       }
+  //     });
+  //   };
+  // };
 
   // A really lightweight plugin wrapper around the constructor,
   // preventing against multiple instantiations
   $.fn[ pluginName ] = function (options) {
-    this.each(function() {
+    this.each(function () {
       if (!$.data(this, "plugin_" + pluginName)) {
         $.data(this, "plugin_" + pluginName, new Plugin(this, options));
       }
